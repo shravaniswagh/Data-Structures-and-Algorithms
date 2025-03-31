feca2b456ac5552921cf1219f150dc1b73aaf365
shravanisw <155523596+shravaniswagh@users.noreply.github.com>
2025-03-31 14:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (67.89%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,7 +4,9 @@ public:
         int left = 0;
         int right = nums.size()-1;
         while(left<right){
+            
             int mid = left + (right - left) / 2;
+
             if(nums[mid]>nums[mid+1]){
                 right = mid;
             }
