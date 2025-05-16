4cad696842a170d275d78d85d021d9001a143842
shravanisw <155523596+shravaniswagh@users.noreply.github.com>
2025-05-16 15:38:22 +0530

    Time: 50 ms (72.12%) | Memory: 18.6 MB (91.78%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -5,7 +5,6 @@ public:
         int m = matrix[0].size();
         int row = 0;
         int col = m-1;
-        int flag = 0;
         while(row<n && col>=0){
             if(matrix[row][col] == target){
                 return true;
