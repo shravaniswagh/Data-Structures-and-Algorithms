58f2e2317e7b8c3c5216645e46bbf251e8a1dd24
shravanisw <155523596+shravaniswagh@users.noreply.github.com>
2025-05-31 13:10:53 +0530

    Time: 1 ms (51.46%) | Memory: 10.5 MB (68.79%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void dfs(int index,vector<int>&nums,vector<int>&current,vector<vector<int>>&ans){
+        ans.push_back(current);
+        for(int i = index;i<nums.size();i++){
+            if(i>index && nums[i] == nums[i-1]) continue;
+            current.push_back(nums[i]);
+            dfs(i + 1,nums,current,ans);
+            current.pop_back();
+        }
+    }    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>current;
+        sort(nums.begin() , nums.end());
+        dfs(0,nums,current,ans);
+        return ans;
+    }
+    
+};
