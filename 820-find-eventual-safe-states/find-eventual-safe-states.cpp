c4fd7b245c4c0c8552acd6917eee56170efbbfc9
shravanisw <155523596+shravaniswagh@users.noreply.github.com>
2026-01-29 22:24:11 +0530

    Time: 57 ms (20.46%) | Memory: 66.8 MB (10.45%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    bool bfs(int j, vector<int>& outdegree, vector<vector<int>>& graph,
-             set<int>& terminated, vector<int>& vis) {
-
-        if (terminated.count(j)) return true;
-        if (vis[j] == 1) return false;  // cycle detected
-        if (vis[j] == 2) return true;   // already known safe
-
-        vis[j] = 1; // visiting
-
-        for (int nei : graph[j]) {
-            if (!bfs(nei, outdegree, graph, terminated, vis)) {
-                return false;
-            }
-        }
-
-        vis[j] = 2;      // safe
-        terminated.insert(j);
-        return true;
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //kahn's algorithm
         int n = graph.size();
-        vector<int> outdegree(n);
-        set<int> terminated;
-        vector<int> vis(n, 0);
-
-        for (int i = 0; i < n; i++) {
+        vector<int>outdegree(n,0);
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++){
             outdegree[i] = graph[i].size();
-            if (outdegree[i] == 0) {
-                terminated.insert(i);
-                vis[i] = 2; // safe
+            for(int j : graph[i]){
+                adj[j].push_back(i);
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            bfs(i, outdegree, graph, terminated, vis);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(outdegree[i] == 0){
+                q.push(i);
+            }
         }
-
-        return vector<int>(terminated.begin(), terminated.end());
+        vector<int>safe(n,0);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safe[node] = 1;
+            for(int p : adj[node]){
+                outdegree[p]--;
+                if(outdegree[p] == 0){
+                    q.push(p);
+                }
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(safe[i])ans.push_back(i);
+        }
+        return ans;
     }
 };
